b0e73f19564140ea86eee8b31a7921818fcca143
Matthew Valverde <matthew.valverde@b-reel.com>
2016-12-30 01:51:17 -0700

    animation

---
+++ Assets/Scripts/WoodTableAnimationScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodTableAnimationScript : MonoBehaviour {
+
+	public Animator table;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void StartTableRotation(){
+		table.Play("TableRotate");
+	}
+}
